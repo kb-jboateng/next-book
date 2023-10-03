d09aa855e01c903f165fe25e0f8ec3ac50423729
blxck <qwasi.jk@gmail.com>
2023-10-02 19:28:01 -0600

    using next auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,3 @@
-import { NextRequest, NextResponse } from "next/server";
+export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/home"] };
-
-export async function middleware(request: NextRequest) {
-    const token = request.cookies.get('next-auth.csrf-token')?.value;
-    if (!token && request.nextUrl.pathname.startsWith('/home')) {
-        request.nextUrl.pathname = '/login';
-        return NextResponse.redirect(request.nextUrl);
-    }
-}
+export const config = { matcher: ["/home"] }
